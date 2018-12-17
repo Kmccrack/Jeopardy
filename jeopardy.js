c6ffb4ab376020f4e7a8e751f5b73ac24af5eb22
Kat <m_kaitavia@yahoo.com>
2018-12-17 16:02:55 -0500

    working on moadl

--- jeopardy.js
+++ jeopardy.js
@@ -1,7 +1,31 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+let score = 0;
+
+$("#one").on("click", function(){
+        swal({
+        text: "Finish the cheer: Awesome! Oh wow! 'Like totally freak me out........'",
+        buttons: {
+            a: "I mean right on!",
+            b: "I said right on!",
+            c: "I totally mean right on!",
+            d: "I totally said right on!",
+        }}
+        )
+       
+        if (swal.a.data("clicked") == true){ 
+        score += $("#one").val();
+        $('#scoreBoard').html(score)}
+        else if(swal.b.data("clicked") == false|| swal.c.data("clicked") == false || swal.d.data("clicked") == false){
+        score -=$("#one").val();
+        }
+        var elementClicked;
+$("element").click(function(){
+   elementClicked = true;
+});
+if( elementClicked != true ) {
+    alert("element not clicked");
+}else{
+    alert("element clicked");
+}
+        
+    })
+
