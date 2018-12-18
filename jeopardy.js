a4c35504cdb2c6cb6f1d2c5c4f9a13f751ae19a8
Kat <m_kaitavia@yahoo.com>
2018-12-18 02:22:09 -0500

    javascript working

--- jeopardy.js
+++ jeopardy.js
@@ -1,41 +1,107 @@
 var score = 0;
 
-document.getElementById("one").addEventListener("click", clickMeOne)
+document.getElementById("one").addEventListener("click", questionOne)
 
-function clickMeOne() {
+function questionOne() {
     swal({
-        text: "Finish the cheer: Awesome! Oh wow! 'Like totally freak me out........'",
+        text: "Kirsten Dunst was a cheerleader before filming 'Bring It On!",
         buttons: {
-            a: "I mean right on!",
-            b: "I said right on!",
-            c: "I totally mean right on!",
-            d: "I totally said right on!",
-        }}
-        )
-         $('.swal-button--a').on('click', () => {
-            score += 100;
-            $('#scoreBoard').html("Score:" + score)
+            a: "True",
+            b: "False",
+            
+        }
+    }
+    )
+    $('.swal-button--a').on('click', () => {
+        score += 100;
+        $('#scoreBoard').html("Score:" + score)
 
-        })
+    })
 
 }
-document.getElementById("two").addEventListener("click", clickMeTwo)
-   
-function clickMeTwo() {
-        swal({
-        text: "What is the name of the 2004 sequal ?",
-        buttons: {
-            a: "Index",
-            b: "Thumb",
-            c: "Middle",
-            d: "Pointer",
-        }}
-        )
-        console.log("working")
-        // $('.swal-button--c').on('click', () => {
-        //     score += 100;
-        //     $('#scoreBoard').html("Score:" + score)
 
-        // })
+document.getElementById("two").addEventListener("click", questionTwo)
+
+function questionTwo() {
+    swal({
+        text: " This movie stars......",
+        buttons: {
+            a: "Kirsten Dunst, Eliza Dushku",
+            b: "Hayden Paniterre, Hilary Duff",
+            c: "Kirsten Dunst, Drew Barrymore",
+            d: "Gabrielle Union, Lindsay Lohan",
+        }
     }
+    )
+    console.log("working")
+    $('.swal-button--c').on('click', () => {
+        score += 100;
+        $('#scoreBoard').html("Score:" + score)
+
+    })
+}
+
+document.getElementById("three").addEventListener("click", questionThree)
+
+function questionThree() {
+    swal({
+        text: "Kirsten Dunst plays....",
+        buttons: {
+            a: "Big Red",
+            b: "Torrance",
+            c: "Missy",
+            d: "Isis",
+        }
+    }
+    )
+    console.log("working")
+    $('.swal-button--c').on('click', () => {
+        score += 100;
+        $('#scoreBoard').html("Score:" + score)
+
+    })
+}
+
+document.getElementById("four").addEventListener("click", questionFour)
+
+function questionFour() {
+    swal({
+        text: " Gabrielle Union plays....",
+        buttons: {
+            a: "LaFred",
+            b: "Jenelope",
+            c: "Pauletta",
+            d: "Isis",
+        }
+    }
+    )
+    console.log("working")
+    $('.swal-button--c').on('click', () => {
+        score += 100;
+        $('#scoreBoard').html("Score:" + score)
+
+    })
+}
+
+document.getElementById("five").addEventListener("click", questionFivw)
+
+function questionFive() {
+    swal({
+        text: " Three girls on the Clovers squad are in a singing group in real life. What is the name of the group?",
+        buttons: {
+            a: "3LW",
+            b: "Destiny's Child",
+            c: "Blaque",
+            d: "702 ",
+        }
+    }
+    )
+    console.log("working")
+    $('.swal-button--c').on('click', () => {
+        score += 100;
+        $('#scoreBoard').html("Score:" + score)
+
+    })
+}
+
 
